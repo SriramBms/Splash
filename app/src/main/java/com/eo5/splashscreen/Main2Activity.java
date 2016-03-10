1773fa6a750c9cfa3f9d71b5b4a95631f38db8b7
SriramBms <sram.bms@gmail.com>
2016-03-09 21:42:31 -0500

    init

---
+++ app/src/main/java/com/eo5/splashscreen/Main2Activity.java
@@ -0,0 +1,24 @@
+package com.eo5.splashscreen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final Intent intent = new Intent(this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+
+    }
+}
